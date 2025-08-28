afbe28a63ab087b56b2192c48d8d61206b7a3312
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-08-28 22:10:35 +0530

    connected to mongo DB

---
+++ src/db/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+    console.log("Mongo URL:", process.env.MONGODB_URL);
+    console.log("DB Name:", DB_NAME);
+
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `Connected to MongoDB database: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.log("Error while connecting to the database", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("hello");
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
+
+
+/*
+import express from 'express';
+const app = express();
+
+;( async () => {
+    try{
+        mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (err) => {
+            console.log("error");
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    }
+    catch(err){
+        console.log("Error while connecting to the database", err);
+        throw err;
+    }
+})()
+*/
